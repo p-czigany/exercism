4fb71d0bf5a12b77ec9d5056c355e847bf658391
Peter Czigany <p.czigany@gmail.com>
2023-11-20 13:57:30 +0100

    refactor case-when to oneline whens

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -3,47 +3,29 @@
 module Blackjack
   def self.parse_card(card)
     case card
-    when 'ace'
-      11
-    when 'two'
-      2
-    when 'three'
-      3
-    when 'four'
-      4
-    when 'five'
-      5
-    when 'six'
-      6
-    when 'seven'
-      7
-    when 'eight'
-      8
-    when 'nine'
-      9
-    when 'ten'
-      10
-    when 'jack'
-      10
-    when 'queen'
-      10
-    when 'king'
-      10
-    else
-      0
+    when 'ace' then 11
+    when 'two' then 2
+    when 'three' then 3
+    when 'four' then 4
+    when 'five' then 5
+    when 'six' then 6
+    when 'seven' then 7
+    when 'eight' then 8
+    when 'nine' then 9
+    when 'ten' then 10
+    when 'jack' then 10
+    when 'queen' then 10
+    when 'king' then 10
+    else 0
     end
   end
 
   def self.card_range(card1, card2)
     case (parse_card card1) + (parse_card card2)
-    when ..11
-      'low'
-    when ..16
-      'mid'
-    when ..20
-      'high'
-    else
-      'blackjack'
+    when ..11 then 'low'
+    when ..16 then 'mid'
+    when ..20 then 'high'
+    else 'blackjack'
     end
   end
 
