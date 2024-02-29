59bd2c183ca0fa92a13cfb4db532af6637b94261
Peter Czigany <p.czigany@gmail.com>
2024-02-29 17:49:13 +0100

    refactor sieve

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,22 +1,31 @@
+# frozen_string_literal: true
+
 class Sieve
   def initialize(upper_limit)
     @upper_limit = upper_limit
   end
 
   def primes
+    possible_primes = (2..@upper_limit).to_a.reverse
     primes = []
-    (2..upper_limit).each do |current|
-      primes << current if prime? current
+    until possible_primes.empty?
+      current = possible_primes.pop
+      next unless prime? current
+
+      possible_primes = remove_multiples(possible_primes, current)
+      primes << current
     end
     primes
   end
-
-  attr_reader :upper_limit
 
   def prime?(current)
     (2..current - 1).each do |possible_divider|
       return false if (current % possible_divider).zero?
     end
     true
+  end
+
+  def remove_multiples(numbers, divider)
+    numbers.filter { |num| num % divider != 0 }
   end
 end
