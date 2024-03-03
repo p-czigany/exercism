919ab2244723bcd73644c03c1b323f91e0174517
Peter Czigany <p.czigany@gmail.com>
2024-03-04 00:58:12 +0100

    introduce constant

--- java/tim-from-marketing/src/main/java/Badge.java
+++ java/tim-from-marketing/src/main/java/Badge.java
@@ -2,11 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Badge {
+  private static final String DEFAULT_DEPT = "OWNER";
+
   public String print(Integer id, String name, String department) {
     List<String> badgeParts = new ArrayList<>();
     if (id != null) badgeParts.add(String.format("[%d]", id));
     badgeParts.add(name);
-    badgeParts.add((department == null) ? "OWNER" : department.toUpperCase());
+    badgeParts.add((department == null) ? DEFAULT_DEPT : department.toUpperCase());
     return String.join(" - ", badgeParts);
   }
 }
