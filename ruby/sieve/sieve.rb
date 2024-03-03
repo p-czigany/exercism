61c02d72ca55dc5fa1401346fc420c8526ce7d37
Peter Czigany <p.czigany@gmail.com>
2024-03-03 17:29:54 +0100

    refactor: rename and format

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -7,21 +7,25 @@ class Sieve
     @upper_limit = upper_limit
   end
 
-  def delete_multiples(arr, prime)
-    (prime**2 - 1...upper_limit).step(prime) { |index| arr[index] = false }
+  def mark_multiples(sieve, prime)
+    (prime**2 - 1...upper_limit).step(prime) do |i|
+      sieve[i] = false
+    end
   end
 
   def collect_primes_from_sieve(primes, sieve)
-    (2..upper_limit).each { |number| primes << number if sieve[number - 1] }
+    (2..upper_limit).each do |number|
+      primes << number if sieve[number - 1]
+    end
   end
 
   def calculate
     primes = []
     sieve = Array.new(upper_limit, true)
-    (1..Integer.sqrt(upper_limit - 1)).each do |index|
-      next unless sieve[index]
+    (1..Integer.sqrt(upper_limit - 1)).each do |i|
+      next unless sieve[i]
 
-      delete_multiples(sieve, index + 1)
+      mark_multiples(sieve, i + 1)
     end
 
     collect_primes_from_sieve(primes, sieve)
