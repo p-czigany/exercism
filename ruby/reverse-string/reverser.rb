4cbeb95c6784a6831da57989094f908fa612426e
Peter Czigany <p.czigany@gmail.com>
2024-03-03 18:41:42 +0100

    add fastest implementation

--- ruby/reverse-string/reverser.rb
+++ ruby/reverse-string/reverser.rb
@@ -1,8 +1,5 @@
 class Reverser
   def self.reverse(string)
-    reversed = ''
-    chars = string.chars
-    string.length.times { reversed << chars.pop }
-    reversed
+    string.reverse!
   end
 end
