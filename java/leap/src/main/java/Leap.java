accffb0e45aaeb95fff8a3590ac7ae25509d1127
Peter Czigany <p.czigany@gmail.com>
2024-03-04 13:03:52 +0100

    refactor leap

--- java/leap/src/main/java/Leap.java
+++ java/leap/src/main/java/Leap.java
@@ -1,14 +1,5 @@
 class Leap {
   boolean isLeapYear(int year) {
-    if (year % 4 == 0) {
-      if (year % 100 == 0) {
-        if (year % 400 == 0) {
-          return true;
-        }
-        return false;
-      }
-      return true;
-    }
-    return false;
+    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
   }
 }
