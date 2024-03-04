b7e26857f1485d4eb79e697aeb19f996132673e7
Peter Czigany <p.czigany@gmail.com>
2024-03-04 13:48:41 +0100

    add high-scores shitty first draft

---
+++ java/high-scores/src/main/java/HighScores.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class HighScores {
+
+  private final List<Integer> highScores;
+
+  public HighScores(List<Integer> highScores) {
+    this.highScores = highScores;
+  }
+
+  List<Integer> scores() {
+    return this.highScores;
+  }
+
+  Integer latest() {
+    return scores().get(scores().size() - 1);
+  }
+
+  Integer personalBest() {
+    return scores().stream().max(Comparator.naturalOrder()).get();
+  }
+
+  List<Integer> personalTopThree() {
+    var sortedHighScores = new ArrayList<>(scores());
+    sortedHighScores.sort(Comparator.reverseOrder());
+    return sortedHighScores.subList(0, Math.min(3, sortedHighScores.size()));
+  }
+}
