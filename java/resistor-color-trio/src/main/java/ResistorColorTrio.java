5c3995dce406edd75dbbb58fc9ae568e7b25950f
Peter Czigany <p.czigany@gmail.com>
2024-03-05 20:49:36 +0100

    refactor: remove static import

--- java/resistor-color-trio/src/main/java/ResistorColorTrio.java
+++ java/resistor-color-trio/src/main/java/ResistorColorTrio.java
@@ -1,24 +1,20 @@
-import static java.lang.String.format;
-
-import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.stream.Stream;
 
 class ResistorColorTrio {
 
-  private static final LinkedHashMap<String, Integer> COLOR_CODES = new LinkedHashMap<>();
-
-  static {
-    COLOR_CODES.put("black", 0);
-    COLOR_CODES.put("brown", 1);
-    COLOR_CODES.put("red", 2);
-    COLOR_CODES.put("orange", 3);
-    COLOR_CODES.put("yellow", 4);
-    COLOR_CODES.put("green", 5);
-    COLOR_CODES.put("blue", 6);
-    COLOR_CODES.put("violet", 7);
-    COLOR_CODES.put("grey", 8);
-    COLOR_CODES.put("white", 9);
-  }
+  private static final Map<String, Integer> COLOR_CODES =
+      Map.of(
+          "black", 0,
+          "brown", 1,
+          "red", 2,
+          "orange", 3,
+          "yellow", 4,
+          "green", 5,
+          "blue", 6,
+          "violet", 7,
+          "grey", 8,
+          "white", 9);
 
   private int colorCode(String color) {
     return COLOR_CODES.get(color);
@@ -41,6 +37,6 @@ class ResistorColorTrio {
   }
 
   String label(String[] colors) {
-    return format("%sohms", convertToNotation(value(colors)));
+    return String.format("%sohms", convertToNotation(value(colors)));
   }
 }
