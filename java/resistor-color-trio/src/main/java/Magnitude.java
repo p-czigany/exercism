109b96b32a4be3e293af385820422ca4e977f574
Peter Czigany <p.czigany@gmail.com>
2024-03-06 10:45:23 +0100

    refactor: rename thousand constant to magnitude

--- java/resistor-color-trio/src/main/java/Magnitude.java
+++ java/resistor-color-trio/src/main/java/Magnitude.java
@@ -8,7 +8,7 @@ public enum Magnitude {
 
   private final int powerOfOneThousand;
   private final String metricPrefix;
-  private static final int ONE_THOUSAND = 1000;
+  private static final int ONE_MAGNITUDE = 1000;
 
   Magnitude(int powerOfOneThousand, String metricPrefix) {
     this.powerOfOneThousand = powerOfOneThousand;
@@ -42,7 +42,7 @@ public enum Magnitude {
   public static Magnitude getMagnitude(long number) {
     return getByPowerOfOneThousand(
         Math.min(
-            (int) Stream.iterate(number, n -> n >= ONE_THOUSAND, n -> n / ONE_THOUSAND).count(),
+            (int) Stream.iterate(number, n -> n >= ONE_MAGNITUDE, n -> n / ONE_MAGNITUDE).count(),
             maxPowerOfOneThousand()));
   }
 
@@ -50,7 +50,7 @@ public enum Magnitude {
     var magnitude = getMagnitude(number);
     return String.format(
         "%d %s",
-        number / (int) Math.pow(ONE_THOUSAND, magnitude.getPowerOfOneThousand()),
+        number / (int) Math.pow(ONE_MAGNITUDE, magnitude.getPowerOfOneThousand()),
         magnitude.getMetricPrefix());
   }
 }
