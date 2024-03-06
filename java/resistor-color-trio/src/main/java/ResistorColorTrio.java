edec6edf0932796c7f6b7833ea6ca2fd58d7a84e
Peter Czigany <p.czigany@gmail.com>
2024-03-06 14:15:54 +0100

    refactor: make enum more specific, less abstract

--- java/resistor-color-trio/src/main/java/ResistorColorTrio.java
+++ java/resistor-color-trio/src/main/java/ResistorColorTrio.java
@@ -1,60 +1,33 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Map;
-import java.util.stream.Stream;
 
 class ResistorColorTrio {
 
-  private enum Magnitude {
-    BASE(0, ""),
-    KILO(1, "kilo"),
-    MEGA(2, "mega"),
-    GIGA(3, "giga");
+  enum ResistanceUnit {
+    OHM(1, "ohms"),
+    KILO_OHM(1000, "kiloohms"),
+    MEGA_OHM(1000_000, "megaohms"),
+    GIGA_OHM(1000_000_000, "gigaohms");
 
-    private final int powerOfOneThousand;
-    private final String metricPrefix;
-    private static final int ONE_MAGNITUDE = 1000;
+    private final long multiplier;
+    private final String unit;
 
-    Magnitude(int powerOfOneThousand, String metricPrefix) {
-      this.powerOfOneThousand = powerOfOneThousand;
-      this.metricPrefix = metricPrefix;
+    ResistanceUnit(int multiplier, String unit) {
+      this.multiplier = multiplier;
+      this.unit = unit;
     }
 
-    private int getPowerOfOneThousand() {
-      return this.powerOfOneThousand;
+    private static ResistanceUnit forValue(long value) {
+      return Arrays.stream(ResistanceUnit.values())
+          .sorted(Comparator.reverseOrder())
+          .filter(u -> value > u.multiplier)
+          .findFirst()
+          .orElse(ResistanceUnit.OHM);
     }
 
-    private String getMetricPrefix() {
-      return this.metricPrefix;
-    }
-
-    private static Magnitude getByPowerOfOneThousand(int powerOfOneThousand) {
-      for (Magnitude magnitude : Magnitude.values()) {
-        if (magnitude.getPowerOfOneThousand() == powerOfOneThousand) {
-          return magnitude;
-        }
-      }
-      throw new IllegalArgumentException("No enum constant with value " + powerOfOneThousand);
-    }
-
-    private static int maxPowerOfOneThousand() {
-      return Stream.of(Magnitude.values())
-          .mapToInt(Magnitude::getPowerOfOneThousand)
-          .max()
-          .orElse(Integer.MIN_VALUE);
-    }
-
-    private static Magnitude getMagnitude(long number) {
-      return getByPowerOfOneThousand(
-          Math.min(
-              (int) Stream.iterate(number, n -> n >= ONE_MAGNITUDE, n -> n / ONE_MAGNITUDE).count(),
-              maxPowerOfOneThousand()));
-    }
-
-    private static String convertToNotation(long number) {
-      var magnitude = getMagnitude(number);
-      return String.format(
-          "%d %s",
-          number / (int) Math.pow(ONE_MAGNITUDE, magnitude.getPowerOfOneThousand()),
-          magnitude.getMetricPrefix());
+    private String format(long value) {
+      return "%d %s".formatted((value / multiplier), unit);
     }
   }
 
@@ -84,6 +57,7 @@ class ResistorColorTrio {
   }
 
   String label(String[] colors) {
-    return String.format("%sohms", Magnitude.convertToNotation(value(colors)));
+    long value = value(colors);
+    return ResistanceUnit.forValue(value).format(value);
   }
 }
