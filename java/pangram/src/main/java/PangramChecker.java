2724d8d407577a4a61cb65bbd3fc66c737acb262
Peter Czigany <p.czigany@gmail.com>
2024-03-06 15:19:20 +0100

    refactor: distinct characters in stream should be exactly all

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,23 +1,9 @@
-import java.util.stream.IntStream;
-
 public class PangramChecker {
 
   private static final int N_OF_LETTERS_IN_ALPHABET = 26;
-  private static final char FIRST_LETTER = 'a';
 
   public boolean isPangram(String input) {
-    boolean[] lettersPresent =
-        input
-            .toLowerCase()
-            .chars()
-            .filter(Character::isLetter)
-            .collect(
-                () -> new boolean[N_OF_LETTERS_IN_ALPHABET],
-                (arr, c) -> arr[c - FIRST_LETTER] = true,
-                (arr1, arr2) -> {});
-
-    return IntStream.range(0, lettersPresent.length)
-        .mapToObj(i -> lettersPresent[i])
-        .allMatch(b -> b);
+    return input.toLowerCase().chars().filter(Character::isLetter).distinct().count()
+        == N_OF_LETTERS_IN_ALPHABET;
   }
 }
