8656966a81e8f8665165e55c4d4da6276f84fe12
Peter Czigany <p.czigany@gmail.com>
2024-03-06 15:13:48 +0100

    refactor: stream api

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,27 +1,23 @@
+import java.util.stream.IntStream;
+
 public class PangramChecker {
 
-  private static final int NO_OF_LETTERS_IN_ALPHABET = 26;
+  private static final int N_OF_LETTERS_IN_ALPHABET = 26;
   private static final char FIRST_LETTER = 'a';
-  private static final char LAST_LETTER = 'z';
 
   public boolean isPangram(String input) {
-    boolean[] letterPresent = new boolean[NO_OF_LETTERS_IN_ALPHABET];
-    var inputLower = input.toLowerCase();
+    boolean[] lettersPresent =
+        input
+            .toLowerCase()
+            .chars()
+            .filter(Character::isLetter)
+            .collect(
+                () -> new boolean[N_OF_LETTERS_IN_ALPHABET],
+                (arr, c) -> arr[c - FIRST_LETTER] = true,
+                (arr1, arr2) -> {});
 
-    for (int i = 0; i < inputLower.length(); i++) {
-      char c = inputLower.charAt(i);
-
-      if (Character.isLetter(c) && c >= FIRST_LETTER && c <= LAST_LETTER) {
-        letterPresent[c - FIRST_LETTER] = true;
-      }
-    }
-
-    for (int i = 0; i < NO_OF_LETTERS_IN_ALPHABET; i++) {
-      if (!letterPresent[i]) {
-        return false;
-      }
-    }
-
-    return true;
+    return IntStream.range(0, lettersPresent.length)
+        .mapToObj(i -> lettersPresent[i])
+        .allMatch(b -> b);
   }
 }
