601edea75de0d3433d56b83e97a8bbd30643e79e
Czigány Péter <czigany.peter@telekom.hu>
2025-01-08 00:59:52 +0100

    add rust anagram solution

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,33 +1,36 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let mut result = HashSet::new();
-    let mut sorted_word: Vec<char> = word
-        .to_lowercase()
-        .chars()
-        .collect::<Vec<_>>()
-        .into_iter()
-        .collect();
-    sorted_word.sort();
+    let not_similar = Box::new(NotEqualsIgnoreCase::not_equals_ignore_case);
+    let anagrams = Box::new(Anagrams::is_anagram);
 
-    for possible_anagram in possible_anagrams {
-        if word.len() == possible_anagram.len() {
-            let mut sorted_candidate: Vec<char> = possible_anagram
-                .to_lowercase()
-                .chars()
-                .collect::<Vec<_>>()
-                .into_iter()
-                .collect();
-            sorted_candidate.sort();
-            if !case_insensitive_equal(word, possible_anagram) && sorted_word == sorted_candidate {
-                result.insert(possible_anagram.clone());
-            }
-        }
-    }
-
-    result
+    possible_anagrams
+        .iter()
+        .filter(|&candidate| (not_similar)(&word, candidate))
+        .filter(|&candidate| (anagrams)(&word, candidate))
+        .cloned()
+        .collect()
 }
 
-fn case_insensitive_equal(word1: &str, word2: &str) -> bool {
-    word1.to_lowercase() == word2.to_lowercase()
+struct Anagrams;
+
+impl Anagrams {
+    fn is_anagram(s1: &str, s2: &str) -> bool {
+        if s1.len() != s2.len() {
+            return false;
+        }
+        let mut chars1: Vec<char> = s1.to_lowercase().chars().collect();
+        let mut chars2: Vec<char> = s2.to_lowercase().chars().collect();
+        chars1.sort_unstable();
+        chars2.sort_unstable();
+        chars1 == chars2
+    }
+}
+
+struct NotEqualsIgnoreCase;
+
+impl NotEqualsIgnoreCase {
+    fn not_equals_ignore_case(s1: &str, s2: &str) -> bool {
+        !s1.to_lowercase().eq(&s2.to_lowercase())
+    }
 }
