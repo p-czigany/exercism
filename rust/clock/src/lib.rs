74a6ca3c78da673eae0d2ea6a0ee10748c3ad58e
Czigány Péter <czigany.peter@telekom.hu>
2025-06-06 12:23:37 +0200

    fix solution for Rust Clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,51 +1,53 @@
-use std::cell::Cell;
 use std::fmt;
+use std::fmt::Formatter;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Clock {
     hours: i32,
     minutes: i32,
-    cached_normal: Cell<Option<(i32, i32)>>,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock {
-            hours,
-            minutes,
-            cached_normal: Cell::new(None),
-        }
+        let total_minutes = hours * 60 + minutes;
+        let (hours, minutes) = (
+            total_minutes.div_euclid(60).rem_euclid(24),
+            total_minutes.rem_euclid(60),
+        );
+        Self { hours, minutes }
     }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
+    pub fn add_minutes(self, minutes: i32) -> Self {
         Self::new(self.hours, self.minutes + minutes)
     }
 
-    fn normal(&self) -> (i32, i32) {
-        self.cached_normal.get().unwrap_or_else(|| {
-            let normalized = (
-                self.total_minutes().div_euclid(60).rem_euclid(24),
-                self.total_minutes().rem_euclid(60),
+    pub fn add_minutes_mut(mut self, minutes: i32) -> Self {
+        self.minutes += minutes;
+        if self.minutes >= 60 || self.minutes <= 0 {
+            (self.hours, self.minutes) = (
+                (self.hours + self.minutes.div_euclid(60)).rem_euclid(24),
+                self.minutes.rem_euclid(60),
             );
-            self.cached_normal.set(Some(normalized));
-            normalized
-        })
+        }
+        self
     }
 
-    fn total_minutes(&self) -> i32 {
-        self.hours * 60 + self.minutes
+    pub fn checked_new(hours: i32, minutes: i32) -> Option<Self> {
+        let total_minutes = hours.checked_mul(60)?.checked_add(minutes)?;
+        let (hours, minutes) = (
+            total_minutes.checked_div_euclid(60)?.checked_rem_euclid(24)?,
+            total_minutes.checked_rem_euclid(60)?,
+        );
+        Some(Self { hours, minutes })
+    }
+
+    pub fn checked_add_minutes(&self, minutes: i32) -> Option<Self> {
+        Self::checked_new(self.hours, self.minutes.checked_add(minutes)?)
     }
 }
 
 impl fmt::Display for Clock {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let (hours, minutes) = self.normal();
-        write!(f, "{:02}:{:02}", hours, minutes)
-    }
-}
-
-impl PartialEq for Clock {
-    fn eq(&self, other: &Self) -> bool {
-        self.normal() == other.normal()
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), fmt::Error> {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
